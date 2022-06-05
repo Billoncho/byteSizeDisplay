b7cb08a457f407a59a3cf7f630c7fed9698eba01
Billy Gene Ridgeway <37519033+Billoncho@users.noreply.github.com>
2022-06-06 07:23:34 +0800

    Add files via upload

---
+++ byteSizeDisplay/main.c
@@ -0,0 +1,19 @@
+/*
+Author: Billy Ridgeway
+Purpose: This programs displays the byte size of: int, char, long, long long, double and long double.
+Date: 21 May, 2022
+*/
+
+int main ()
+{
+    printf("Size of (int) = %zd bytes\n", sizeof(int));
+    printf("Size of (short) = %zd bytes\n", sizeof(short));
+    printf("Size of (char) = %zd bytes\n", sizeof(char));
+    printf("Size of (long) = %zd bytes\n", sizeof(long));
+    printf("Size of (long long) = %zd bytes\n", sizeof(long long));
+    printf("Size of (float) = %zd bytes\n", sizeof(float));
+    printf("Size of (double) = %zd bytes\n", sizeof(double));
+    printf("Size of (long double) = %zd bytes\n", sizeof(long double));
+
+    return 0;
+}
